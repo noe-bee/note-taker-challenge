fe73ed83fa8cff89136f10809b431794962b99c1
The Other Promise <150410378+noe-bee@users.noreply.github.com>
2024-03-12 09:43:57 -0700

    get notes route created

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const fs = require('fs');
 const path = require('path');
 const express = require('express');
+const api = require('./routes/index.js');
 //
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -8,6 +8,7 @@ const app = express();
 //middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
 
 app.use(express.static('public'));
 
@@ -19,4 +20,13 @@ res.sendFile(path.join(__dirname, '/public/index.html'))
 //GET route for notes.html
 app.get('/notes', (req, res) => 
 res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('/product', (req, res) => {
+    return res.sendFile(path.join(__dirname, '/public/product.html'))
+})
+
+//listen
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
 );
